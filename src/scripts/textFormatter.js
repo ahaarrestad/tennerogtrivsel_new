711be73b8ec161f2909177f5559ec4bdc873d280
Asbjorn <asbjorn@aarrestad.com>
2026-02-01 12:29:58 +0100

    Ryddet bort unødvendig æøå håndtering

--- src/scripts/textFormatter.js
+++ src/scripts/textFormatter.js
@@ -7,16 +7,7 @@ export function formatInfoText(rawText) {
     const emailRegex = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
     const phoneRegex = /((\+47|0047)?\s*[2-9]\d{1}\s*\d{2}\s*\d{2}\s*\d{2})/g;
 
-    // 2. Sikre korrekt håndtering av norske tegn
-    // Vi mapper de vanligste særnorske tegnene til deres HTML-entiteter
-    // Dette er valgfritt i moderne UTF-8, men gjør koden ekstremt robust.
-    const charMap = {
-        'æ': '&aelig;', 'ø': '&oslash;', 'å': '&aring;',
-        'Æ': '&AElig;', 'Ø': '&Oslash;', 'Å': '&Aring;'
-    };
-
     return rawText
-        .replace(/[æøåÆØÅ]/g, (match) => charMap[match]) // html encode norske tegn
         .replace(emailRegex, '<a href="mailto:$1" class="text-blue-600">$1</a>') // linkify e-post
         .replace(phoneRegex, (match) => {
             // match er det fulle nummeret med mellomrom, f.eks. "51 42 33 33"
