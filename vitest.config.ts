b534a1180ed2f1b49c8542273f55a252696733d8
Asbjorn <asbjorn@aarrestad.com>
2026-02-15 11:56:03 +0100

    ryddet i test-filene

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node', // or 'jsdom' if you need browser APIs
-    include: ['src/**/*.test.ts', 'src/**/*.test.js'], // or wherever your tests are
+    include: ['src/**/__tests__/**/*.{ts,js}'], // or wherever your tests are
     // workaround for astro:content
     alias: {
       'astro:content': new URL('./src/__mocks__/astroContent.ts', import.meta.url).pathname,
